2c4705aadf9fddca148e3d14bd83ccc64a5848b3
Benjamin Kane <benjamin.kane@news.co.uk>
2022-12-09 13:51:17 +0000

    clean up day three

--- src/test/java/DayThreeTest.java
+++ src/test/java/DayThreeTest.java
@@ -1,4 +1,30 @@
-import junit.framework.TestCase;
-public class DayThreeTest extends TestCase {
-  
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;import junit.framework.TestCase;
+
+public class DayThreeTest  {
+
+    private DayThree dayThree;
+
+    @Before
+    public void setup() {
+        this.dayThree = new DayThree(input);
+    }
+    @Test
+    public void givenInput_returnsScoreOf_157() {
+        assertEquals(157, dayThree.partOne());
+    }
+
+    @Test
+    public void givenInput_returnsScoreOf_70() {
+        assertEquals(70, dayThree.partTwo());
+    }
+
+    private static final String input = "vJrwpWtwJgWrhcsFMMfFFhFp\n" +
+                                        "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL\n" +
+                                        "PmmdzqPrVvPwwTWBwg\n" +
+                                        "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn\n" +
+                                        "ttgJtRGJQctTZtZT\n" +
+                                        "CrZsJsPPZsGzwwsLwLmpwMDw";
 }
+
